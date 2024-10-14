d62c990d6361c1b9aa54b34398f0f596da82f7a0
sangam5756 <sangammunde3@gmail.com>
2024-10-14 20:22:30 +0530

    nextconfig change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export', // This enables static export
+  };
+  
+  export default nextConfig;
+  
