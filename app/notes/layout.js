f6b1ff8541bea026426d7bb256b4da93639cd243
sangam5756 <sangammunde3@gmail.com>
2024-11-10 20:57:22 +0530

    notes componet added

---
+++ app/notes/layout.js
@@ -0,0 +1,12 @@
+import "../globals.css";
+import React from "react";
+
+const layout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default layout;
